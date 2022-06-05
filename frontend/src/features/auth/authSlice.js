cb219e1816a1c4dee64483db435158c41e2f4722
ozan isik <ozan_isik14@hotmail.com>
2022-06-05 16:43:50 -0400

    [build][frontend] start user registration

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import authService from './authService'
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem('user'))
@@ -10,6 +11,21 @@ const initialState = {
     isLoading: false,
     message: ''
 }
+
+//Register User // async thunk fucntions
+//createAsyncThunk takes in a string as the action and a function that takes in a user and thinkAPI
+//user is sent in throug the register page
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+    try {
+        return await authService.register(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) ||
+                        error.message || error.toString()
+        
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
 
 export const authSlice = createSlice({
     name: 'auth',
