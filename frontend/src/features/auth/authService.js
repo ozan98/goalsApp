ba1a6d180082667d10811a56967c55e91b6a6c22
ozan isik <ozan_isik14@hotmail.com>
2022-06-05 18:33:30 -0400

    [build][frontend] start user login, extra reducer for login, hook up log in form

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -14,6 +14,18 @@ const register = async (userData) => {
     return response.data
 }
 
+// Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+    
+    // Check if the response worked(sent data back) and store response data in localStorage
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+    
+    return response.data
+}
+
 // Logout user
 const logout = () => {
     localStorage.removeItem('user')
@@ -22,6 +34,7 @@ const logout = () => {
 const authService = {
     register,
     logout,
+    login,
 }
 
 export default authService
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
+// useSelector is used to select from the state (e.g user, isLoading, isError)
+// useDispatch is for dispatching a function (e.g register, reset, api thunk functions in reducer)
+import {useSelector, useDispatch} from 'react-redux'
+//to redirect
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {FaUser} from 'react-icons/fa'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -9,6 +18,26 @@ function Login() {
     })
 
     const {email, password} = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    
+    // What we want from our state
+    // Must specify what part of the state we are trying to retrieve from
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        if(isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -19,6 +48,16 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        const userData = {
+            email,
+            password,
+        }
+        dispatch(login(userData))
+    }
+
+    if(isLoading) {
+        <Spinner />
     }
 
     return (
